e5f2dd1470a2fda3fd54602f2ac0308d0bc6b7f5
Rise Crystal <risecrystal0@gmail.com>
2024-04-28 16:04:07 +0700

    Commit otomatis: Sun Apr 28 16:04:06 SEAST 2024

--- uploadgit.sh
+++ uploadgit.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-# Menambahkan semua file ke dalam .gitignore
-ls >> .gitignore
+# Tanggal hari ini
+tanggal=$(date)
+
+# Data history yang akan ditambahkan
+data_history="# Perubahan pada: $tanggal"
+echo "$data_history" >> .gitignore
+
+# Mendapatkan daftar file yang ada dalam direktori saat ini
+file_list=$(ls)
+
+# Iterasi melalui setiap file dalam daftar
+while IFS= read -r file; do
+    # Memeriksa apakah file sudah ada dalam .gitignore
+    if ! grep -q "^$file$" .gitignore; then
+        # Jika tidak, tambahkan file ke .gitignore
+        echo "$file" >> .gitignore
+    fi
+done <<< "$file_list"
 
 # Hapus baris yang kosong dari .gitignore
 sed -i '/^[[:space:]]*$/d' .gitignore
@@ -10,7 +26,7 @@ sed -i '/^[[:space:]]*$/d' .gitignore
 git add .
 
 # Commit perubahan dengan pesan
-git commit -m "Commit otomatis: $(date)"
+git commit -m "Commit otomatis: $tanggal"
 
 # Push perubahan ke GitHub
 git push origin main
