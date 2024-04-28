41b0ee7e35f3f2e316470ab41a6d17527bf4f052
Rise Crystal <risecrystal0@gmail.com>
2024-04-28 15:47:56 +0700

    Commit otomatis: Sun Apr 28 15:47:56 SEAST 2024

--- uploadgit.sh
+++ uploadgit.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+# Tanggal hari ini
+tanggal=$(date)
+
+# Data history yang akan ditambahkan
+data_history="# Perubahan pada: $tanggal"
+
+# Periksa apakah file .gitignore sudah ada
+if [ -f .gitignore ]; then
+  # Cek apakah history sudah ada di dalam file .gitignore
+  if grep -q "$data_history" ".gitignore"; then
+    echo "History sudah ada."
+  else
+    # Tambahkan history ke dalam file .gitignore
+    echo "$data_history" >> .gitignore
+    echo "History berhasil ditambahkan ke dalam file .gitignore."
+  fi
+else
+  # Buat file .gitignore dan tambahkan history
+  echo "$data_history" > .gitignore
+  echo "File .gitignore baru telah dibuat dan history berhasil ditambahkan."
+fi
+
 # Tambahkan semua perubahan ke dalam commit
 git add .
 
 # Commit perubahan dengan pesan
-git commit -m "Commit otomatis: $(date)"
+git commit -m "Commit otomatis: $tanggal"
 
 # Push perubahan ke GitHub
 git push origin main
