037c5b85c334e80b2d6583f4ef5f17eb456e122c
Rise Crystal <risecrystal0@gmail.com>
2024-04-28 15:55:56 +0700

    Menambahkan solid.txt ke .gitignore secara otomatis

--- uploadgit.sh
+++ uploadgit.sh
@@ -1,38 +1,16 @@
 #!/bin/bash
 
-# Tanggal hari ini
-tanggal=$(date)
-
-# Data history yang akan ditambahkan
-data_history="# Perubahan pada: $tanggal"
-
-# Periksa apakah file .gitignore sudah ada
-if [ -f .gitignore ]; then
-  # Cek apakah history sudah ada di dalam file .gitignore
-  if grep -q "$data_history" ".gitignore"; then
-    echo "History sudah ada."
-  else
-    # Tambahkan history ke dalam file .gitignore
-    echo "$data_history" >> .gitignore
-    echo "History berhasil ditambahkan ke dalam file .gitignore."
-  fi
-else
-  # Buat file .gitignore dan tambahkan history
-  echo "$data_history" > .gitignore
-  echo "File .gitignore baru telah dibuat dan history berhasil ditambahkan."
-fi
-
 # Tambahkan file/folder yang sudah di-upload ke dalam .gitignore
-git ls-files --cached --ignored --exclude-standard | grep -v '^#' >> .gitignore
+echo "solid.txt" >> .gitignore
 
-# Hapus baris yang tidak mengandung karakter dari .gitignore
+# Hapus baris yang kosong dari .gitignore
 sed -i '/^[[:space:]]*$/d' .gitignore
 
 # Tambahkan semua perubahan ke dalam commit
 git add .
 
 # Commit perubahan dengan pesan
-git commit -m "Commit otomatis: $tanggal"
+git commit -m "Menambahkan solid.txt ke .gitignore secara otomatis"
 
 # Push perubahan ke GitHub
 git push origin main
