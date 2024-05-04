d3e1322226b6473bfdb425b36cec254688178245
rise-crystal <risecrystal0@gmail.com>
2024-05-04 16:32:46 +0700

    Commit otomatis: Sat, May  4, 2024  4:32:41 PM

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 pkg update && pkg upgrade -y
 apt update && apt upgrade -y
-pkg install nodejs
-pkg install ffmpeg
-pkg install git
-pkg install tesseract
+pkg install nodejs -y
+pkg install ffmpeg -y
+pkg install git -y
+pkg install tesseract -y
 npm install
 npm start
