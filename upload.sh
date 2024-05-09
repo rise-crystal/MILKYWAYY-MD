31d3217959d1d7a44e27f0568e1f418e03822305
rise-crystal <risecrystal0@gmail.com>
2024-05-09 14:48:08 +0700

    Thu, May  9, 2024  2:48:08 PM

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Tambahkan isi dari perintah ls ke dalam file .gitignore jika belum ada
-ls --ignore=".gitignore" >> .gitignore
+ls -d --ignore=".gitignore" */ >> .gitignore
 
 # Eksekusi git add untuk semua perubahan
 git add .
