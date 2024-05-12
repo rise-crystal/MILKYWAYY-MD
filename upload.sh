4047e389e16c580978d43bd6dc63482f3006cafc
rise-crystal <risecrystal0@gmail.com>
2024-05-12 18:21:11 +0700

    Update: Sun May 12 18:21:11 SEAST 2024

--- upload.sh
+++ upload.sh
@@ -6,12 +6,12 @@ ls -d --ignore=".gitignore" --ignore="node_modules" */ >> .gitignore
 # Eksekusi git add untuk semua perubahan kecuali folder node_modules
 git add . -- ':!node_modules'
 
-# Lakukan commit dengan pesan yang berisi tanggal dan waktu saat ini
-commit_message=$(date)
+# Lakukan commit dengan pesan yang lebih deskriptif
+commit_message="Update: $(date)"
 git commit -m "$commit_message"
 
 # Tarik perubahan terbaru dari branch utama di remote repository
-git pull origin main
+git pull origin main || { echo "Gagal menarik perubahan dari remote repository"; exit 1; }
 
 # Dorong perubahan ke branch utama di remote repository
-git push -u origin main
+git push -u origin main || { echo "Gagal mendorong perubahan ke remote repository"; exit 1; }
