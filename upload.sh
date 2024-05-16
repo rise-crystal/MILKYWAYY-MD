f960475547b6a308863076b94ef5ea27b85b2c8e
rise-crystal <risecrystal0@gmail.com>
2024-05-16 08:29:41 +0700

    Update: Thu May 16 08:29:41 SEAST 2024

--- upload.sh
+++ upload.sh
@@ -18,8 +18,8 @@ git pull origin main || { echo "Gagal menarik perubahan dari remote repository '
 # Dorong perubahan ke branch utama di remote repository 'origin'
 git push -u origin main || { echo "Gagal mendorong perubahan ke remote repository 'origin'"; exit 1; }
 
-# Tarik perubahan dari remote repository 'github'
-git pull github main || { echo "Gagal menarik perubahan dari remote repository 'github'"; exit 1; }
+# Tarik perubahan dari remote repository 'github' dengan flag --allow-unrelated-histories
+git pull github main --allow-unrelated-histories || { echo "Gagal menarik perubahan dari remote repository 'github'"; exit 1; }
 
 # Dorong perubahan ke branch utama di remote repository 'github'
 git push -u github main || { echo "Gagal mendorong perubahan ke remote repository 'github'"; exit 1; }
